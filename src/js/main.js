37f1496d2990fd90ca37057eafd65764d0bfa577
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-01-11 18:12:27 +0200

    added animation to photos in Inst section

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,25 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(
+  ".inst__img",
+  {
+    width: "0%",
+  },
+  {
+    scrollTrigger: {
+      trigger: ".inst",
+      start: "top 50%",
+    },
+    duration: 2.5,
+    stagger: 0.2,
+    width: "100%",
+    ease: "power4.out",
+  }
+);
+
 document.addEventListener("DOMContentLoaded", function (event) {
-
-
-
-  
   console.log("DOM fully loaded and parsed");
 });
