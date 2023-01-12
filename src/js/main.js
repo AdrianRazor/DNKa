264c56f859cdc3b266c8ea0b4001ee5bcbb37ec6
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-01-12 17:48:01 +0200

    added submenu and transitions in the header

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,75 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// Instagram section -> photos appearing animation
-gsap.fromTo(
-  ".inst__img",
-  {
-    width: "0%",
-    scale: 1.4,
-  },
-  {
-    scrollTrigger: {
-      trigger: ".inst",
-      start: "top 50%",
-    },
-    duration: 2.5,
-    stagger: 0.2,
-    width: "100%",
-    scale: 1,
-    ease: "power4.out",
-  }
-);
-
 document.addEventListener("DOMContentLoaded", function (event) {
+  // Header submenus
+  const submenu = document.querySelectorAll(".submenu");
+
+  if (submenu) {
+    submenu.forEach((item) => {
+      item.addEventListener("click", () => {
+        submenu.forEach((el) => {
+          el.classList.remove("open");
+        });
+
+        item.classList.toggle("open");
+      });
+    });
+
+    window.addEventListener("click", (event) => {
+      if (
+        !event.target.closest(".submenu") ||
+        event.target.classList.contains("submenu__item")
+      ) {
+        submenu.forEach((item) => {
+          item.classList.remove("open");
+        });
+      }
+    });
+  }
+
+  // Aside navigation
+  const burger = document.querySelector(".menu__item--burger");
+  const asideMenu = document.querySelector(".aside");
+  const menuBottom = document.querySelector(".menu--bottom");
+
+  if (burger) {
+    const rootElement = document.documentElement;
+
+    burger.addEventListener("click", () => {
+      asideMenu.classList.add("open");
+      menuBottom.classList.add("hide");
+      rootElement.classList.add("block");
+    });
+
+    window.addEventListener("click", (event) => {
+      if (event.target.classList.contains("aside")) {
+        asideMenu.classList.remove("open");
+        menuBottom.classList.remove("hide");
+        rootElement.classList.remove("block");
+      }
+    });
+  }
+
+  // Instagram section -> photos appearing animation
+  gsap.fromTo(
+    ".inst__img",
+    {
+      width: "0%",
+      scale: 1.4,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".inst",
+        start: "top 50%",
+      },
+      duration: 2.5,
+      stagger: 0.2,
+      width: "100%",
+      scale: 1,
+      ease: "power4.out",
+    }
+  );
+
   console.log("DOM fully loaded and parsed");
 });
