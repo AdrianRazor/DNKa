4b60ee7ce9157d31316e8223c28d34155fc4c3ff
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-01-12 12:38:46 +0200

    added fixed mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// Instagram section -> photos appearing animation
 gsap.fromTo(
   ".inst__img",
   {
     width: "0%",
+    scale: 1.4,
   },
   {
     scrollTrigger: {
@@ -16,6 +18,7 @@ gsap.fromTo(
     duration: 2.5,
     stagger: 0.2,
     width: "100%",
+    scale: 1,
     ease: "power4.out",
   }
 );
