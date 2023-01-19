7a8dcd3c5c5f31bf7d741bec3ae4e5b87f11543d
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-01-19 15:47:16 +0200

    created Catalog Page

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // Header submenus
   const submenu = document.querySelectorAll(".submenu");
 
-  if (submenu) {
+  if (submenu?.length) {
     submenu.forEach((item) => {
       item.addEventListener("click", () => {
         if (!item.classList.contains("open")) {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const asideMenu = document.querySelector(".aside");
   const menuBottom = document.querySelector(".menu--bottom");
 
-  if (burger) {
+  if (burger && asideMenu && menuBottom) {
     const rootElement = document.documentElement;
 
     burger.addEventListener("click", () => {
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // Link's hover
   const link = document.querySelectorAll(".line-hover");
 
-  if (link) {
+  if (link?.length) {
     link.forEach((el) => {
       el.addEventListener("mouseenter", () => {
         el.classList.add("active");
@@ -70,126 +70,185 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
   // Start section animation
+  const start = document.querySelector(".start");
+  const startImg = document.querySelectorAll(".start__img");
+  const startTitle = document.querySelectorAll(".start__text h1 span");
+  const startSubtitle = document.querySelector(".start__text p");
+  const startBtn = document.querySelector(".start__btn");
   const startAnimation = gsap.timeline();
-  startAnimation
-    .from(".start__img", {
-      duration: 1.5,
-      ease: "power3.out",
-      scale: 0,
-    })
-    .from(
-      ".start__img",
-      {
-        duration: 2.5,
-        ease: "elastic.out(1, 0.2)",
-        rotation: 20,
-      },
-      "-=1.5"
-    )
-    .from(
-      ".start__text h1 span",
-      {
-        duration: 2.5,
-        ease: "power3.out",
-        xPercent: -140,
-        opacity: 0,
-        stagger: 0.3,
-      },
-      "-=1.8"
-    )
-    .from(
-      ".start__text p",
-      {
+
+  if (
+    start &&
+    startImg?.length &&
+    startTitle?.length &&
+    startSubtitle &&
+    startBtn
+  ) {
+    startAnimation
+      .from(startImg, {
         duration: 1.5,
         ease: "power3.out",
-        xPercent: -100,
-        opacity: 0,
-      },
-      "-=1"
-    )
-    .from(
-      ".start__btn",
-      {
-        duration: 1.2,
-        ease: "elastic.out(1, 0.4)",
         scale: 0,
-      },
-      "-=2"
-    );
+      })
+      .from(
+        startImg,
+        {
+          duration: 2.5,
+          ease: "elastic.out(1, 0.2)",
+          rotation: 20,
+        },
+        "-=1.5"
+      )
+      .from(
+        startTitle,
+        {
+          duration: 2.5,
+          ease: "power3.out",
+          xPercent: -140,
+          opacity: 0,
+          stagger: 0.3,
+        },
+        "-=1.8"
+      )
+      .from(
+        startSubtitle,
+        {
+          duration: 1.5,
+          ease: "power3.out",
+          xPercent: -100,
+          opacity: 0,
+        },
+        "-=1"
+      )
+      .from(
+        startBtn,
+        {
+          duration: 1.2,
+          ease: "elastic.out(1, 0.4)",
+          scale: 0,
+        },
+        "-=2"
+      );
+  }
 
   // Ticker animation
-  gsap.to(".stripe-to-left", {
-    xPercent: -100,
-    repeat: -1,
-    duration: 40,
-    ease: "linear",
-  });
-  gsap.from(".stripe-to-right", {
-    xPercent: -100,
-    repeat: -1,
-    duration: 40,
-    ease: "linear",
-  });
+  const stripeLeft = document.querySelectorAll(".stripe-to-left");
+  const stripeRight = document.querySelectorAll(".stripe-to-right");
+
+  if (stripeLeft?.length) {
+    gsap.to(stripeLeft, {
+      xPercent: -100,
+      repeat: -1,
+      duration: 40,
+      ease: "linear",
+    });
+  }
+
+  if (stripeRight?.length) {
+    gsap.from(stripeRight, {
+      xPercent: -100,
+      repeat: -1,
+      duration: 40,
+      ease: "linear",
+    });
+  }
 
   // Slider on main page
-  const swiperMain = new Swiper(".swiperMain", {
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 8000,
-    },
+  const swiperExist = document.querySelectorAll(".swiper");
 
-    breakpoints: {
-      375: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-        spaceBetween: 15,
+  if (swiperExist?.length) {
+    const swiperMain = new Swiper(".swiperMain", {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 8000,
       },
-      1023: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 0,
-      },
-      1700: {
-        slidesPerView: 4,
-        slidesPerGroup: 4,
-      },
-    },
 
-    pagination: {
-      el: ".latest__pagination",
-      bulletClass: "pagination__bullet",
-      bulletActiveClass: "pagination__bullet--active",
-      clickable: true,
-    },
+      breakpoints: {
+        375: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 15,
+        },
+        1023: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 0,
+        },
+        1700: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+        },
+      },
 
-    navigation: {
-      nextEl: ".latest__btn-next",
-      prevEl: ".latest__btn-prev",
-    },
-  });
+      pagination: {
+        el: ".latest__pagination",
+        bulletClass: "pagination__bullet",
+        bulletActiveClass: "pagination__bullet--active",
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: ".latest__btn-next",
+        prevEl: ".latest__btn-prev",
+      },
+    });
+  }
 
   // Instagram section -> photos appearing animation
-  gsap.fromTo(
-    ".inst__img",
-    {
-      width: "0%",
-      scale: 1.4,
-    },
-    {
-      scrollTrigger: {
-        trigger: ".inst",
-        start: "top 50%",
+  const inst = document.querySelector(".inst");
+  const instImg = document.querySelectorAll(".inst__img");
+
+  if (inst && instImg?.length) {
+    gsap.fromTo(
+      instImg,
+      {
+        width: "0%",
+        scale: 1.4,
       },
-      duration: 2.5,
-      stagger: 0.2,
-      width: "100%",
-      scale: 1,
-      ease: "power4.out",
-    }
-  );
+      {
+        scrollTrigger: {
+          trigger: inst,
+          start: "top 50%",
+        },
+        duration: 2.5,
+        stagger: 0.2,
+        width: "100%",
+        scale: 1,
+        ease: "power4.out",
+      }
+    );
+  }
+
+  // Filter
+  const filter = document.querySelectorAll(".filter");
+  const filterButton = document.querySelectorAll(".filter__btn");
+
+  if (filter?.length && filterButton?.length) {
+    filterButton.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        // if (!btn.classList.contains("open")) {
+        //   submenu.forEach((el) => {
+        //     el.classList.remove("open");
+        //   });
+        // }
+
+        filter[index].classList.toggle("open");
+      });
+    });
+
+    window.addEventListener("click", (event) => {
+      if (!event.target.closest(".filter")) {
+        filter.forEach((item) => {
+          item.classList.remove("open");
+        });
+      }
+    });
+  }
+
+  // Tabs
 
   console.log("DOM fully loaded and parsed");
 });
